5f8ac57762c2f8eb636d09305b1390e105c398a6
Houssam <houssamah10@gmail.com>
2023-11-10 10:37:11 +0200

    second commt

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/students', function (req, res) {
+  var student = [
+    {id: 1, name: "houssam" },
+    {id: 2, name: "ali" },
+    {id: 3, name: "ahmad" },
+    {id: 4, name: "elia" }
+  ]
+    
+  
+})
 
 app.listen(PORT)
