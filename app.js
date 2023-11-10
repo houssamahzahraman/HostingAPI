dec5ff3950fbcbd94925452d718578a18029d018
Houssam <houssamah10@gmail.com>
2023-11-10 10:51:48 +0200

    4th commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const PORT = process.env.PORT || 3000;
+app.use(cors())
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
